4157b7f04b6d7f9db4991450b73cfde7e9d32020
Temlin Olivér <git@teml.in>
2019-05-13 03:58:27 +0200

    add win/loss messaging

--- Gomoku_client/src/main/Game.java
+++ Gomoku_client/src/main/Game.java
@@ -2,16 +2,16 @@ package main;
 
 public class Game implements Runnable {
 	public Menu menu;
-    public static void main(String[] args) {
-        Menu menu = new Menu();
-    }
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+	}
 
-    @Override
-    public void run() {
-        Window window = new Window(menu);
-    }
-    public void addmenu(Menu menu) //gives the menu to the game so it can pass it to the window
-    {
-    	this.menu=menu;
-    }
+	@Override
+	public void run() {
+		Window window = new Window(menu);
+	}
+	public void addmenu(Menu menu) //gives the menu to the game so it can pass it to the window
+	{
+		this.menu=menu;
+	}
 }
